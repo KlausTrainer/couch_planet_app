0c72abcf2f41ffc0a9f88c216d291e0a454e5461
Klaus Trainer <klaus_trainer@posteo.de>
2012-02-11 22:51:49 +0100

    Remove archives stuff
    We don't use it and don't need it anymore.

--- views/archives-by-topic/map.js
+++
@@ -1,5 +0,0 @@
-function(doc) {
-  if (doc.type == "Activity Stream" && doc.verb == "post") {
-    emit([doc.provider.id, new Date(doc.postedTime)], [doc.title, doc.object.id])
-  }
-}
--- views/archives/map.js
+++
@@ -1,4 +0,0 @@
-function(doc) {
-  if (doc.type == "Activity Stream" && doc.verb == "post")
-    emit(new Date(doc.postedTime), [doc.title, doc.object.id])
-}
