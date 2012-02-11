fa3e5a772a7b8a71deb045e9d261a9e8d8268c4a
Klaus Trainer <klaus_trainer@posteo.de>
2012-02-11 17:17:45 +0100

    Redesign and some overall improvements
    Many thanks to Juan Pablo Pincheira for the great new design!
    
    Contains some overall improvements and cleanups in the code.

--- _attachments/script/index.js
+++ _attachments/script/index.js
@@ -9,26 +9,23 @@ $.couch.app(function(app) {
     if (ceaseFire)
       return false
 
-    var last_entry = $("#posts li.entry:last")[0]
+    var $last_entry = $("#posts li.entry:last")
 
     if (!this.prev_date)
-      this.prev_date= last_entry.getAttribute("date")
+      this.prev_date = $last_entry.data('date')
 
-    var pathname = document.location.pathname
-    var viewname = pathname.substring(pathname.lastIndexOf("/") + 1)
+    var viewname = document.location.pathname.substring(document.location.pathname.lastIndexOf("/") + 1),
+        descending = $(document).getUrlParam("descending") || "true",
+        start = $(document).getUrlParam("startkey"),
+        end_date = descending == "true" ?  "0000-01-01T00%3A00%3A00.000Z" : "9999-12-31T23%3A59%3A59.999Z"
 
-    var descending = $(document).getUrlParam("descending") || "true"
-
-    var start = $(document).getUrlParam("startkey")
-    var end_date = descending == "true" ?  "0000-01-01T00%3A00%3A00.000Z" : "9999-12-31T23%3A59%3A59.999Z"
-
-    var date = last_entry.getAttribute("date")
+    var date = $last_entry.data("date")
     if (start && viewname == "recent-posts-by-topic") {
       var topic = JSON.parse(unescape(start))[0]
       var startkey = '["' + topic + '","' + date + '"]'
       var endkey = '["' + topic + '","' + end_date + '"]'
       if (date == this.prev_date) {
-        var docid = last_entry.getAttribute("id")
+        var docid = $last_entry.attr("id")
         startkey = startkey + '&startkey_docid=' + '"' + docid + '"'
       }
       this.prev_date = date
@@ -36,7 +33,7 @@ $.couch.app(function(app) {
       var startkey = '"' + date + '"'
       var endkey = '"' + end_date + '"'
       if (date == this.prev_date) {
-        var docid = last_entry.getAttribute("id")
+        var docid = $last_entry.attr("id")
         startkey = startkey + '&startkey_docid=' + '"' + docid + '"'
       }
       this.prev_date = date
@@ -44,50 +41,47 @@ $.couch.app(function(app) {
 
     var url = "../../_view/" + viewname + "?descending=" + descending +
               "&limit=5&skip=1&startkey=" + startkey + "&endkey=" + endkey
-    var xhr = $.ajax({
+    $.ajax({
       url : url,
-      async: false
-    })
-
-    var data = null
-    if ((xhr.status == 200 || xhr.status == 304))
-      data = JSON.parse(xhr.responseText)
-
-    if (data && data.rows) {
-      if (data.offset == data.total_rows || data.rows.length < 4)
-        ceaseFire = true
-      var entries = data.rows
-      var entriesXML = ""
-      entries.forEach(function(entryObj) {
-        var entry = entryObj.value
-        if (entry.enclosure) {
-          var enclosure = '<p class="enclosure"><img class="type-icon"' +
-                      ' src="../../images/audio.png"/> <a href="' +
-                      entry.enclosure_link + '">' + entry.enclosure +
-                      '</a></p>'
-        } else {
-          var enclosure = ""
+      dataType: 'json',
+      success: function(data) {
+        if (data && data.rows) {
+          ceaseFire = (data.offset == data.total_rows || data.rows.length < 4)
+          data.rows.forEach(function(entryObj) {
+            var entry = entryObj.value, enclosure = '', newEntry
+            if (entry.enclosure) {
+              enclosure = '<p class="enclosure"><img class="type-icon"' +
+                          ' src="../../images/audio.png"/> <a href="' +
+                          entry.enclosure_link + '">' + entry.enclosure +
+                          '</a></p>'
+            }
+            newEntry = $('<li id="' + entry.link + '" data-date="' +
+                        entry.date + '" class="entry"><h3><a href="' +
+                        entry.providerUrl + '">' + entry.providerName +
+                        '</a></h3><h4><a href="' + entry.link + '">' +
+                        entry.title + '</a></h4>' + '<div class ="body">' +
+                        entry.body + enclosure + '<p class="by">by ' +
+                        entry.author + ', <span class="date">' +
+                        $.prettyDate(entry.date) + '</span></p></div></li>')
+            newEntry.find('div.body img,iframe').each(function() {
+              var $i = $(this)
+              if ($i.length && !$i.hasClass('type-icon')) {
+                $i.replaceWith($("<div class='center'></div>").append($i.clone()))
+              }
+            })
+            $('#posts').append(newEntry)
+          })
         }
-        entriesXML += '<li id="' + entry.link + '" date="' + entry.date +
-                      '" class="entry"><h3><a href="' + entry.providerUrl +
-                      '">' + entry.providerName + '</a></h3><h4><a href="' +
-                      entry.link + '">' + entry.title + '</a></h4>' +
-                      '<div class ="body">' + entry.body + enclosure +
-                      '<p class="by">by ' + entry.author +
-                      ', <span class="date">' + $.prettyDate(entry.date) +
-                      '</span></p></div></li>'
-      })
-      return entriesXML
-    } else {
-      return false
-    }
+        return false
+      }
+    })
+    return false
   }
 
   $(document).endlessScroll({
-    bottomPixels: 400,
-    fireOnce: false,
-    fireDelay: 500,
-    data: scrollFun,
+    bottomPixels: 256,
+    fireDelay: 128,
+    callback: scrollFun,
     loader: '<img id="loader" src="../../images/loading.gif"/>',
     insertAfter: "#posts li.entry:last",
     ceaseFire: function() { return ceaseFire }
