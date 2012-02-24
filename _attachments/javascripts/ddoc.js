ce2333a9867df73a0f4330b6b5723accad9db5ee
Klaus Trainer <klaus_trainer@posteo.de>
2012-02-24 22:26:51 +0100

    Enable filtering by subscribed feed

--- _attachments/javascripts/ddoc.js
+++ _attachments/javascripts/ddoc.js
@@ -1,31 +1,62 @@
-$.couch.app.ddoc = {
-  evently: {
-    posts: {
-      _changes: {
-        query: {limit: 8, descending: true, view: 'recent-posts'},
-        data: function(data) {
-          var ret = {}, posts = []
-          data.rows.forEach(function(row) {
-            var post = row.value
-            posts.push({
-              title: post.title,
-              author: post.author,
-              providerName: post.providerName,
-              providerUrl: post.providerUrl,
-              enclosure_link: post.enclosure_link,
-              enclosure: post.enclosure,
-              date: post.date,
-              pretty_date: $.prettyDate(post.date),
-              body: post.body,
-              link: post.link
-            })
+$.couch.app.ddoc = {}
+
+$.couch.app.ddoc.getSubscription = function() {
+  var m0 = window.location.toString().match(/^[^#]*(#!.+)$/), m1
+  m1 = m0 ? m0[1].match(/^#!\/subscriptions\/(.+)$/) : null
+  return m1 ? m1[1] : null
+}
+
+$.couch.app.ddoc.getView = function() {
+  var sbscrptn = $.couch.app.ddoc.getSubscription()
+  return sbscrptn ? 'recent-posts-by-topic' : 'recent-posts'
+}
+
+$.couch.app.ddoc.getEndKey = function() {
+  var sbscrptn = $.couch.app.ddoc.getSubscription()
+  return sbscrptn ? [sbscrptn, "0000-01-01T00%3A00%3A00.000Z"] : "0000-01-01T00%3A00%3A00.000Z"
+}
+
+$.couch.app.ddoc.getStartKey = function() {
+  var sbscrptn = $.couch.app.ddoc.getSubscription()
+  return sbscrptn ? [sbscrptn, "3012-12-12T23%3A59%3A59.999Z"] : "3012-12-12T23%3A59%3A59.999Z"
+}
+
+$.couch.app.ddoc.getQuery = function() {
+  return {
+    view: $.couch.app.ddoc.getView(),
+    limit: 8,
+    descending: true,
+    endkey: $.couch.app.ddoc.getEndKey(),
+    startkey: $.couch.app.ddoc.getStartKey()
+  }
+}
+
+$.couch.app.ddoc.evently = {
+  posts: {
+    _changes: {
+      query: $.couch.app.ddoc.getQuery(),
+      data: function(data) {
+        var ret = {}, posts = []
+        data.rows.forEach(function(row) {
+          var post = row.value
+          posts.push({
+            title: post.title,
+            author: post.author,
+            providerName: post.providerName,
+            providerUrl: '#!/subscriptions/' + post.providerUrl,
+            enclosure_link: post.enclosure_link,
+            enclosure: post.enclosure,
+            date: post.date,
+            pretty_date: $.prettyDate(post.date),
+            body: post.body,
+            link: post.link
           })
-          ret.posts = posts
-          return ret
-        },
-        mustache: '{{#posts}}<li id="{{link}}" data-date="{{date}}" class="entry"><h3><a href="{{providerUrl}}">{{providerName}}</a></h3><h4><a href="{{link}}">{{title}}</a></h4><div class="body"><div class="main">{{{body}}}</div>{{#enclosure}}<p class="enclosure"><img class="type-icon" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABHNCSVQICAgIfAhkiAAAAjZJREFUOI3F0T2IFWcUxvH/eWfmeufO7C6667psUEFkg2CVQgnYBUQEBUFjVkQsxA+sLNIkIFaClbXEwsJOkjI2CYRACgu10UJYRFn2xtW9+3W/Zuad9xwby3trT3meww8eDnztkVHLizfnHkXYd2Y4HJGpiDh1Gogc4lQQRCPR6FM8CrDazp849UNzdnqByfwbsnSaJMlxLiLUJUW1SW/4gQcP78+MBADmZvfSSlPyPGci20OeztNIJijKDt3+Mq00B2c2FtjsvsPrNpXv0hu02dGYQojwdZ/Sb1FWGxCQsYCv+/z+x7+9wUBFAqKGQxBTdXEzqn88ezw1dDwAxvZmyGvi/VRbVdGYqpsMfUHq88JvmYIGGw+YKSLCkwftZcCg/yXZYPHabtQCoOLGAWo1ZtidO6NfrVYDxnhAa0TgH0bfqHoCXypcuLL7ZNKUX2KXH40bLRmW68Gkxgm20EZmrs4eTyO5m2U7DwgS9YYbqlaAIvFPN+YvJC78dub0udbCwWP0h21WO8+jje4SBnTd3K09U1O3Ly3+nO3aOU9/uMr/nWds95cQkLgh4cq3h7NWYa94836FolxnWK7hJMYMcxJ+PfL9vuzj9t982KwZVh287+NcgiEuDqb/vV0aHJmcbGdptkrtlV4v0OsOUFWXNOTTysryRKPpRcRRFiWd9QFrawNvZk4AFq/P3IuILgfVaYfzUSLvDH0RKv3TU79uJjseh6CHnLjK1IKL3Utf1X+5OH76GR2mFFD4ymXXAAAAAElFTkSuQmCC"/> <a href="{{enclosure_link}}">{{enclosure}}</a></p>{{/enclosure}}<p class="by">by {{author}}, <span class="date">{{pretty_date}}</span></p></div></li>{{/posts}}',
-        render: 'prepend'
-      }
+        })
+        ret.posts = posts
+        return ret
+      },
+      mustache: '{{#posts}}<li id="{{link}}" data-date="{{date}}" class="entry"><h3><a href="{{providerUrl}}">{{providerName}}</a></h3><h4><a href="{{link}}">{{title}}</a></h4><div class="body"><div class="main">{{{body}}}</div>{{#enclosure}}<p class="enclosure"><img class="type-icon" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABHNCSVQICAgIfAhkiAAAAjZJREFUOI3F0T2IFWcUxvH/eWfmeufO7C6667psUEFkg2CVQgnYBUQEBUFjVkQsxA+sLNIkIFaClbXEwsJOkjI2CYRACgu10UJYRFn2xtW9+3W/Zuad9xwby3trT3meww8eDnztkVHLizfnHkXYd2Y4HJGpiDh1Gogc4lQQRCPR6FM8CrDazp849UNzdnqByfwbsnSaJMlxLiLUJUW1SW/4gQcP78+MBADmZvfSSlPyPGci20OeztNIJijKDt3+Mq00B2c2FtjsvsPrNpXv0hu02dGYQojwdZ/Sb1FWGxCQsYCv+/z+x7+9wUBFAqKGQxBTdXEzqn88ezw1dDwAxvZmyGvi/VRbVdGYqpsMfUHq88JvmYIGGw+YKSLCkwftZcCg/yXZYPHabtQCoOLGAWo1ZtidO6NfrVYDxnhAa0TgH0bfqHoCXypcuLL7ZNKUX2KXH40bLRmW68Gkxgm20EZmrs4eTyO5m2U7DwgS9YYbqlaAIvFPN+YvJC78dub0udbCwWP0h21WO8+jje4SBnTd3K09U1O3Ly3+nO3aOU9/uMr/nWds95cQkLgh4cq3h7NWYa94836FolxnWK7hJMYMcxJ+PfL9vuzj9t982KwZVh287+NcgiEuDqb/vV0aHJmcbGdptkrtlV4v0OsOUFWXNOTTysryRKPpRcRRFiWd9QFrawNvZk4AFq/P3IuILgfVaYfzUSLvDH0RKv3TU79uJjseh6CHnLjK1IKL3Utf1X+5OH76GR2mFFD4ymXXAAAAAElFTkSuQmCC"/> <a href="{{enclosure_link}}">{{enclosure}}</a></p>{{/enclosure}}<p class="by">by {{author}}, <span class="date">{{pretty_date}}</span></p></div></li>{{/posts}}',
+      render: 'prepend'
     }
   }
 }
--- _attachments/javascripts/index.js
+++ _attachments/javascripts/index.js
@@ -1,12 +1,19 @@
 $.couch.app(function(app) {
-  $('#posts').evently(app.ddoc.evently.posts, app)
+  var $nav = $('#nav-header'),
+      $nav_home = $nav.find('a:first'),
+      $posts = $('#posts')
+
+  if (!$.couch.app.ddoc.getSubscription())
+    $nav_home.addClass('nav-selected')
+
+  $posts.evently(app.ddoc.evently.posts, app)
 
   var template = $.couch.app.ddoc.evently.posts._changes.mustache,
       loader = '<img id="loader" src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH+GkNyZWF0ZWQgd2l0aCBhamF4bG9hZC5pbmZvACH5BAAKAAAAIf8LTkVUU0NBUEUyLjADAQAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQACgABACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkEAAoAAgAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkEAAoAAwAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkEAAoABAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQACgAFACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQACgAGACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAAKAAcALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA=="/>',
-      insertAfter = '#posts li.entry:last',
-      bottomPixels = 768, fireDelay = 256, fired = false, ceaseFire = false
+      bottomPixels = 768, fireDelay = 256, fired = false, ceaseFire = false,
+      date, prev_date
 
-  $(document).scroll(function() {
+  var scrollFun = function() {
     if (ceaseFire)
       return // scroll will still get called, but nothing will happen
 
@@ -14,27 +21,49 @@ $.couch.app(function(app) {
 
     if (is_scrollable && fired != true) {
       fired = true
-      $(insertAfter).after('<div id="endless_scroll_loader">' + loader + '</div>')
 
-      var $last_entry = $('#posts li.entry:last'),
-          date = $last_entry.data('date'), start
 
-      if (!this.prev_date || date === this.prev_date) {
-        start = 'startkey="' + date + '"' + '&startkey_docid=' + '"' + $last_entry.attr('id') + '"'
+      var date, start, end, url,
+          query = $.couch.app.ddoc.evently.posts._changes.query,
+          $last_entry = $posts.find('li.entry:last')
+
+      if ($last_entry) {
+        $last_entry.after('<div id="endless_scroll_loader">' + loader + '</div>')
+        date = $last_entry.data('date')
       } else {
-        start = 'skip=1&startkey="' + date + '"'
+        date = "3012-12-12T23%3A59%3A59.999Z"
       }
-      this.prev_date = date
+
+      if (!prev_date || date === prev_date) {
+        if (query.view === 'recent-posts') {
+          start = 'startkey="' + date + '"&startkey_docid=' + '"' + $last_entry.attr('id') + '"'
+        } else {
+          start = 'startkey=["' + query.startkey[0] + '","' + date + '"]' + '&startkey_docid=' + '"' + $last_entry.attr('id') + '"'
+        }
+      } else {
+        if (query.view === 'recent-posts') {
+          start = 'skip=1&startkey="' + date + '"'
+        } else {
+          start = 'skip=1&startkey=["' + query.startkey[0] + '","' + date + '"]'
+        }
+      }
+      prev_date = date
+
+      if (query.view === 'recent-posts') {
+        end = 'endkey="' + query.endkey + '"'
+      } else {
+        end = 'endkey=["' + query.endkey[0] + '","' + query.endkey[1] + '"]'
+      }
 
       $.ajax({
-        url : url = '_view/recent-posts?descending=true&limit=9&' + start + '&endkey="0000-01-01T00%3A00%3A00.000Z"',
+        url: '_view/' + query.view + '?descending=true&limit=' + query.limit + '&' + start + '&' + end,
         dataType: 'json',
         complete: function(xhr, status) {
           if (status === 'success') {
             var data = $.parseJSON(xhr.responseText)
             if (data && data.rows) {
               ceaseFire = (data.offset == data.total_rows || data.rows.length < 8)
-              $(insertAfter).after('<div id="endless_scroll_data">' + $.mustache(template, $.couch.app.ddoc.evently.posts._changes.data(data)) + '</div>')
+              $last_entry.after('<div id="endless_scroll_data">' + $.mustache(template, $.couch.app.ddoc.evently.posts._changes.data(data)) + '</div>')
               $('div#endless_scroll_data').hide().fadeIn()
               $('div#endless_scroll_data').removeAttr('id')
               $('body').after('<div id="endless_scroll_marker"></div>')
@@ -49,7 +78,35 @@ $.couch.app(function(app) {
         }
       })
     }
-  })
+  }
+
+  $(document).scroll(scrollFun)
+
+  var resetView = function() {
+    $('*').off()
+    $(document).off()
+    $(document).scroll(scrollFun)
+    $.couch.app.ddoc.evently.posts._changes.query = $.couch.app.ddoc.getQuery()
+    $posts.children().remove()
+    $posts.evently(app.ddoc.evently.posts, app)
+    initPaths()
+    return true
+  }
+
+  var initPaths = function() {
+    $nav.pathbinder('home', '/')
+    $nav.on('home', function() {
+      $nav_home.addClass('nav-selected')
+      resetView()
+    })
+    $posts.pathbinder('subscriptions', '/subscriptions/:id')
+    $posts.on('subscriptions', function() {
+      $nav_home.removeClass('nav-selected')
+      resetView()
+    })
+  }
+
+  initPaths()
 },
 {
   db: 'couch_planet',
