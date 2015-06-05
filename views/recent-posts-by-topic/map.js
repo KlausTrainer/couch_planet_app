80fc3a8cf7ef7bddec4b2b25ca027ce28f2a0e23
Klaus Trainer <klaus_trainer@posteo.de>
2015-06-05 12:57:01 +0200

    Improve view code

--- views/recent-posts-by-topic/map.js
+++ views/recent-posts-by-topic/map.js
@@ -1,25 +1,36 @@
 function(doc) {
-  if (doc.type == "Activity Stream" && doc.verb == "post") {
-    var ret = {}
-    var date = new Date(doc.postedTime)
-    ret.date = date
-    ret.title = doc.title
-    ret.link = doc.object.id
-    ret.body = doc.object.summary
-    ret.providerName = doc.provider.name
-    ret.providerUrl = doc.provider.id
-    if (!doc.actor.name)
-      ret.author = ret.providerName
-    else
-      ret.author = doc.actor.name
-    if (!doc.actor.link)
-      ret.authorUrl = ret.providerUrl
-    else
-      ret.authorUrl = doc.actor.link
-    if (doc.object.objectType == "podcast") {
-      ret.enclosure = doc.object.id.substr(doc.object.id.lastIndexOf("/") + 1)
-      ret.enclosure_link = doc.object.link
+    'use strict';
+
+    if (doc.type != "Activity Stream" || doc.verb != "post") {
+      return;
     }
-    emit([doc.provider.id, date], ret)
+
+    var post = {},
+        date = new Date(doc.postedTime);
+
+    post.date = date;
+    post.title = doc.title;
+    post.link = doc.object.id;
+    post.body = doc.object.summary;
+    post.providerName = doc.provider.name;
+    post.providerUrl = doc.provider.id;
+
+    if (!doc.actor.name) {
+      post.author = post.providerName;
+    } else{
+      post.author = doc.actor.name;
+    }
+
+    if (!doc.actor.link) {
+      post.authorUrl = post.providerUrl;
+    } else {
+      post.authorUrl = doc.actor.link;
+    }
+
+    if (doc.object.objectType == "podcast") {
+      post.enclosure = doc.object.id.substr(doc.object.id.lastIndexOf("/") + 1);
+      post.enclosure_link = doc.object.link;
+    }
+
+    emit([post.providerUrl, post.date], post)
   }
-}
